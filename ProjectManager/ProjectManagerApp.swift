cd10ddfb80b8b1978094e79bc428274215168fc5
mikio <43379090+mikiolight@users.noreply.github.com>
2025-09-12 23:26:55 +0900

    Initial Commit

---
+++ ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Mikihisa Saito on 2025/09/12.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectManagerTests/ProjectManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerTests.swift
+//  ProjectManagerTests
+//
+//  Created by Mikihisa Saito on 2025/09/12.
+//
+
+import Testing
+@testable import ProjectManager
+
+struct ProjectManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
