d80618ae68e485d7f1919b97043887c1d49b183d
mikio <43379090+mikiolight@users.noreply.github.com>
2025-09-14 01:07:36 +0900

    Add Model file

---
+++ ProjectManager/Model/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  ProjectManager
+//
+//  Created by Mikihisa Saito on 2025/09/14.
+//
+
+import Foundation
---
+++ ProjectManager/Model/Project.swift
@@ -0,0 +1,8 @@
+//
+//  Project.swift
+//  ProjectManager
+//
+//  Created by Mikihisa Saito on 2025/09/14.
+//
+
+import Foundation
